fefe27202e9807b8eccfcb57d68ba6feb1a5b4ec
myrriame <myrriametiffany@gmail.com>
2021-05-31 08:45:36 -0400

    added functionality

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 
+
 function Note(props) {
+
+    function handleClick() {
+        props.onDelete(props.id);
+    }
+
     return (
         <div className="note">
             <h1>
@@ -10,6 +16,7 @@ function Note(props) {
             <p>
                 {props.content}
             </p>
+            <button onClick={handleClick}>DELETE</button>
         </div>
     )
 }
